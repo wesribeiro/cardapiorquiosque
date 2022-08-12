dae538dfe95def95bf86f583835a01c2f476130d
wesribeiro <61030755+wesribeiro@users.noreply.github.com>
2022-08-12 01:21:41 -0300

    -back to top added

--- main.js
+++
@@ -1,9 +0,0 @@
-const toTop = document.querySelector(".to-top");
-
-window.addEventListener("scoll", () => {
-  if (window.pageYOffset > 100) {
-  toTop.classList.add("active");
-} else {
-  toTop.classList.remove("active");
-}
-})
